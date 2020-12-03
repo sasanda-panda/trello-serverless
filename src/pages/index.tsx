6e12b3bfa6c2e473380ce14d3fd29997816741b2
sasanda-panda <eishis-sasai@eishis-sasai-mac.local>
2020-12-04 03:16:55 +0900

    Add: amplify base

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+// import Head from 'next/head'
+import { NextPage } from 'next'
+import styles from '../styles/Home.module.scss'
+
+const Home: NextPage = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home

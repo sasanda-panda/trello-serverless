a32748cc5f378f3ded2ec2007b0d9583f14a1b14
sasanda-panda <eishis-sasai@eishis-sasai-mac.local>
2020-12-05 01:30:28 +0900

    Add: auth logic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,13 @@
+import Amplify from '@aws-amplify/core'
+import API from '@aws-amplify/api'
+import PubSub from '@aws-amplify/pubsub'
+import awsconfig from '../aws-exports'
 import { AppProps } from 'next/app'
 import '../styles/globals.scss'
+
+Amplify.configure(awsconfig)
+API.configure(awsconfig)
+PubSub.configure(awsconfig)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,5 +1,13 @@
+import Auth from '@aws-amplify/auth'
+import { useEffect, useState } from 'react'
 import { NextPage } from 'next'
+import { useRouter } from 'next/router'
 import styles from '../styles/Profile.module.scss'
+
+type AuthenticatedUserType = {
+  email: string,
+  email_verified: boolean
+}
 
 const Profile: NextPage = () => {
 
@@ -7,8 +15,106 @@ const Profile: NextPage = () => {
   // true:  ユーザー情報を表示
   // false: フォームを表示
 
-  return (
-    <div>Profile</div>
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false)
+  const [authenticatedUser, setAuthenticatedUser] = useState<AuthenticatedUserType>({ email: '', email_verified: false })
+  const [authenticationScene, setAuthenticationScene] = useState<'signUp'|'confirmSignUp'|'signIn'>('signIn')
+  const [username, setUsername] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [code, setCode] = useState<string>('')
+
+  const fetchUser = async () => {
+    try {
+      const user = await Auth.currentAuthenticatedUser()
+      setIsAuthenticated(true)
+      setAuthenticatedUser({ email: user.attributes.email, email_verified: user.attibutes.email_verified })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    fetchUser()
+  }, [])
+
+  const router = useRouter()
+
+  const signUp = async () => {
+    try {
+      await Auth.signUp({
+        username,
+        password,
+        attributes: { email: username }
+      })
+      setPassword('')
+      setAuthenticationScene('confirmSignUp')
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const confirmSignUp = async () => {
+    try {
+      await Auth.confirmSignUp(username, code)
+      setAuthenticationScene('signIn')
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const resendSignUp = async () => {
+    try {
+      await Auth.resendSignUp(username)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const signIn = async () => {
+    try {
+      await Auth.signIn(username, password)
+      router.reload()
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const signOut = async () => {
+    try {
+      await Auth.signOut()
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return isAuthenticated ? (
+    <div>
+      <p>Profile - authenticated</p>
+      <div>
+        <button onClick={() => signOut()}>signOut</button>
+      </div>
+    </div>
+  ) : (
+    <div>
+      <p>Profile - not authenticated</p>
+      <div>
+        {(() => {
+          switch(authenticationScene) {
+            case 'signUp':
+              return (
+                <div></div>
+              )
+            case 'confirmSignUp':
+              return (
+                <div></div>
+              )
+            default:
+              return (
+                <div></div>
+              )
+          }
+        })}
+      </div>
+    </div>
   )
 }
 
